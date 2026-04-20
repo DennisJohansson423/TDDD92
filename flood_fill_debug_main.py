406616999c2b8a9e16ccbc52d46a3072da06595e
Dennis Johansson <dennisjohansson@Dennis-MacBook-Air.local>
2026-04-20 14:45:11 +0200

    chore add .gitignore

--- flood_fill_debug_main.py
+++
@@ -1,7 +0,0 @@
-from modules.flood_fill import FloodFill
-from visualdebugger.visualize_flood_fill import VisualizeFloodFill
-
-if __name__ == "__main__":
-    flood_fill = FloodFill()
-    vizualise_flood_fill = VisualizeFloodFill(flood_fill, cell_size=5, alongside_game=False)
-    vizualise_flood_fill.display_map()
